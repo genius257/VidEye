b41b12f63a1f7bd86a9a596aec34b5aed0e6ef1d
Anders Pedersen <hot415@hotmail.com>
2023-05-25 21:15:17 +0200

    Added react component class props type declaration

--- src/components/PosterGrid.tsx
+++ src/components/PosterGrid.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./PosterGrid.css";
 
-export default class PosterGrid extends React.Component {
+export default class PosterGrid extends React.Component<React.PropsWithChildren> {
     /**
      * render
      */
