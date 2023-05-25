bc49530c094edf97f1bcc70ea7683720242be961
Anders Pedersen <hot415@hotmail.com>
2023-05-25 21:14:44 +0200

    Changed indentation from 2 to 4 spaces

--- src/components/PosterGrid.tsx
+++ src/components/PosterGrid.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./PosterGrid.css";
 
 export default class PosterGrid extends React.Component {
-  /**
-   * render
-   */
-  public render() {
-    return (
-      <div className="postergrid" {...this.props}>
-        {this.props.children}
-      </div>
-    );
-  }
+    /**
+     * render
+     */
+    public render() {
+        return (
+            <div className="postergrid" {...this.props}>
+                {this.props.children}
+            </div>
+        );
+    }
 }
