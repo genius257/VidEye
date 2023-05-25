01a8a8f4aa015c364048efd84ebd4b2395f070c8
Anders Pedersen <hot415@hotmail.com>
2023-05-25 21:23:15 +0200

    Changed indentation from 2 to 4 spaces

--- src/models/Series.ts
+++ src/models/Series.ts
@@ -1,54 +1,54 @@
 import {
-  Model,
-  ProxyIt
+    Model,
+    ProxyIt
 } from "../../merchant/Illuminate/Database/Eloquent/Model";
 
 export class Series extends Model {
   fillable = ["title"];
 
-  /**
-   * Check if Series has been watched 100%
-   */
-  isWatched(): boolean {
-    return false;
-  }
+    /**
+     * Check if Series has been watched 100%
+     */
+    isWatched(): boolean {
+        return false;
+    }
 
-  /**
-   * Check if series has been partially watched
-   */
-  inProgress(): boolean {
-    return false;
-  }
+    /**
+     * Check if series has been partially watched
+     */
+    inProgress(): boolean {
+        return false;
+    }
 
-  /**
-   * Check if series has been partially or 100% watched
-   */
-  isWatchedOrInProgress(): boolean {
-    return this.isWatched() || this.inProgress();
-  }
+    /**
+     * Check if series has been partially or 100% watched
+     */
+    isWatchedOrInProgress(): boolean {
+        return this.isWatched() || this.inProgress();
+    }
 
-  getUnwatched(): any[] {
-    return [];
-  }
+    getUnwatched(): any[] {
+        return [];
+    }
 
-  getUnwatchedCount() {
-    return this.getUnwatched().length;
-  }
+    getUnwatchedCount() {
+        return this.getUnwatched().length;
+    }
 
-  getProgress() {
-    return (
-      this.getAttribute("progress") ||
-      this.setAttribute(
-        "progress",
-        (<typeof Series>this.constructor)
-          .resolveConnection("youtube")
-          .table("videos")
-          .where("id", this.YTID)
-          .where("part", "contentDetails")
-          .first()
-      ).getAttribute("progress")
-    );
-  }
+    getProgress() {
+        return (
+            this.getAttribute("progress") ||
+            this.setAttribute(
+                "progress",
+                (<typeof Series>this.constructor)
+                    .resolveConnection("youtube")
+                    .table("videos")
+                    .where("id", this.getAttribute("YTID"))
+                    .where("part", "contentDetails")
+                    .first()
+            ).getAttribute("progress")
+        );
+    }
 }
 
 export default ProxyIt(Series);
@@ -66,5 +66,5 @@ console.log("Z.$resolver", Z.getConnectionResolver());
 const instance = Z.newModelInstance();
 const collection = instance.newCollection();
 [{}, {}, {}].forEach((document) => {
-  collection.push(instance.newFromBuilder(document));
+    collection.push(instance.newFromBuilder(document));
 });
