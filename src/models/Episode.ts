00f51a3f3dca0d7b538153ee578a37f1cca9e616
Anders Pedersen <hot415@hotmail.com>
2023-05-25 21:20:11 +0200

    Changed indentation from 2 to 4 spaces

--- src/models/Episode.ts
+++ src/models/Episode.ts
@@ -1,37 +1,38 @@
 import {
-  Model,
-  ProxyIt
+    Model,
+    ProxyIt
 } from "../../merchant/Illuminate/Database/Eloquent/Model";
 
 export class Episode extends Model {
-  protected $fillable = ["title"];
+    protected $fillable = ["title"];
 
-  isWatched() {
-    //
-  }
+    isWatched(): boolean {
+        return false; //FIXME: implement
+    }
 
-  inProgress() {
-    //
-  }
+    inProgress() {
+        //
+    }
 
-  isWatchedOrInProgress() {
-    return this.isWatched() || this.inProgress();
-  }
+    isWatchedOrInProgress() {
+        return this.isWatched() || this.inProgress();
+    }
 
-  getProgress() {
-    return (
-      this.getAttribute("progress") ||
-      this.setAttribute(
-        "progress",
-        this.constructor
-          .resolveConnection("youtube")
-          .table("videos")
-          .where("id", this.YTID)
-          .where("part", "contentDetails")
-          .first()
-      ).getAttribute("progress")
-    );
-  }
+    getProgress() {
+        return (
+            this.getAttribute("progress") ||
+            this.setAttribute(
+                "progress",
+                this.constructor
+                    //@ts-expect-error
+                    .resolveConnection("youtube")
+                    .table("videos")
+                    .where("id", this.getAttribute("YTID"))
+                    .where("part", "contentDetails")
+                    .first()
+            ).getAttribute("progress")
+        );
+    }
 }
 
 export default ProxyIt(Episode);
