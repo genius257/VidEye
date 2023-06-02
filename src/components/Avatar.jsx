70f3c7068388260846f83477db00cf5753073994
Anders Pedersen <hot415@hotmail.com>
2023-06-02 21:41:36 +0200

    Update Avatar.jsx

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,16 +1,24 @@
+import md5 from "blueimp-md5";
 import React from "react";
 import "./Avatar.css";
 
 export default class Avatar extends React.Component {
-  render() {
-    const photoURL = this.props?.auth?.user?.photoURL;
-    const displayName = this.props?.auth?.user?.displayName;
-    return (
-      <div
-        className="avatar"
-        style={{ backgroundImage: `url(${photoURL})` }}
-        title={displayName}
-      />
-    );
-  }
+    render() {
+        const email = this.props?.auth?.user?.email;
+        const hash =
+            email == null
+                ? "00000000000000000000000000000000"
+                : md5(email.trim().toLowerCase());
+        const photoURL = `https://www.gravatar.com/avatar/${hash}`;
+
+        //const photoURL = this.props?.auth?.user?.photoURL;
+        const displayName = this.props?.auth?.user?.displayName;
+        return (
+            <div
+                className="avatar"
+                style={{ backgroundImage: `url(${photoURL})` }}
+                title={displayName}
+            />
+        );
+    }
 }
