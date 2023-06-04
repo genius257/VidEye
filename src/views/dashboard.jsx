30ad5160ce8f3fb0a912ad2b1dac20bf02dac1e4
Anders Pedersen <hot415@hotmail.com>
2023-06-05 01:20:38 +0200

    Changed dashboard from firebase to supabase

--- src/views/dashboard.jsx
+++ src/views/dashboard.jsx
@@ -7,176 +7,204 @@ import History from "../history";
 import config from "../Firebase/config";
 import Card from "../card";
 import Poster from "../components/Poster";
+import supabase from "../Supabase";
 
 export default class Dashboard extends React.Component {
-  state = {
-    series: {},
-    watched: []
-  };
+    state = {
+        series: {},
+        watched: []
+    };
 
-  componentDidMount() {
-    try {
-      let db = firebase.firestore();
-      db.collection("series")
-        .get()
-        .then((querySnapshot) => {
-          var series = {};
-          querySnapshot.forEach(function (doc) {
-            // doc.data() is never undefined for query doc snapshots
-            //console.log(doc.id, " => ", doc.data());
-            series[doc.id] = doc.data();
-            //series.push(doc.data());
-          });
-          this.setState({ series }, () => this.loadWatched());
-        })
-        .catch((error) => {
-          console.error("Error getting documents: ", error);
-        });
-    } catch (e) {
-      console.error(
-        "Firebase is not currently available.\nThis could be due to transpiling.\nPlease try and reload if the problem persists"
-      );
-    }
-  }
+    componentDidMount() {
+        supabase
+            .from("series")
+            .select("*")
+            .then((result) => {
+                const series = {};
+                result.data.forEach((_series) => {
+                    series[_series.id] = _series;
+                });
+                this.setState({ series } /*, () => this.loadWatched()*/);
+            });
 
-  loadWatched() {
-    let watched = History.getUnwatched();
-    let jobs = [];
-    let result = [];
-    Object.keys(watched).forEach((series) =>
-      Object.keys(watched[series]).forEach((season) =>
-        Object.keys(watched[series][season]).forEach((episode) => {
-          let YTID = this.state.series[series].seasons[season][episode].YTID;
-          //TODO: currently the order of the watched state array value is in an undertermanistic order due to async behavior. An index needs to be assigned, upon promise creation, to assign the result to the appropriate index.
-          jobs.push(
-            fetch(
-              `https://www.googleapis.com/youtube/v3/videos?id=${YTID}&part=contentDetails&key=${config.apiKey}`
-            )
-              .then((response) => response.json())
-              .then((json) =>
-                result.push({
-                  series,
-                  season,
-                  episode,
-                  time: watched[series][season][episode]["time"],
-                  totalTime: Math.max(
-                    moment
-                      .duration(json?.items?.[0]?.contentDetails?.duration ?? 0)
-                      .asSeconds(),
-                    watched[series][season][episode]["duration"]
-                  )
+        /*
+        try {
+            let db = firebase.firestore();
+            db.collection("series")
+                .get()
+                .then((querySnapshot) => {
+                    var series = {};
+                    querySnapshot.forEach(function (doc) {
+                        // doc.data() is never undefined for query doc snapshots
+                        //console.log(doc.id, " => ", doc.data());
+                        series[doc.id] = doc.data();
+                        //series.push(doc.data());
+                    });
+                    this.setState({ series }, () => this.loadWatched());
                 })
-              )
-          );
-        })
-      )
-    );
-    return Promise.all(jobs).then((values) =>
-      this.setState({ watched: result })
-    );
-  }
+                .catch((error) => {
+                    console.error("Error getting documents: ", error);
+                });
+        } catch (e) {
+            console.error(
+                "Firebase is not currently available.\nThis could be due to transpiling.\nPlease try and reload if the problem persists"
+            );
+        }
+        */
+    }
 
-  componentDidCatch(error, errorInfo) {
-    // You can also log the error to an error reporting service
-    console.log(error, errorInfo);
-  }
+    loadWatched() {
+        let watched = History.getUnwatched();
+        let jobs = [];
+        let result = [];
+        Object.keys(watched).forEach((series) =>
+            Object.keys(watched[series]).forEach((season) =>
+                Object.keys(watched[series][season]).forEach((episode) => {
+                    let YTID =
+                        this.state.series[series].seasons[season][episode].YTID;
+                    //TODO: currently the order of the watched state array value is in an undertermanistic order due to async behavior. An index needs to be assigned, upon promise creation, to assign the result to the appropriate index.
+                    jobs.push(
+                        fetch(
+                            `https://www.googleapis.com/youtube/v3/videos?id=${YTID}&part=contentDetails&key=${config.apiKey}`
+                        )
+                            .then((response) => response.json())
+                            .then((json) =>
+                                result.push({
+                                    series,
+                                    season,
+                                    episode,
+                                    time: watched[series][season][episode][
+                                        "time"
+                                    ],
+                                    totalTime: Math.max(
+                                        moment
+                                            .duration(
+                                                json?.items?.[0]?.contentDetails
+                                                    ?.duration ?? 0
+                                            )
+                                            .asSeconds(),
+                                        watched[series][season][episode][
+                                            "duration"
+                                        ]
+                                    )
+                                })
+                            )
+                    );
+                })
+            )
+        );
+        return Promise.all(jobs).then((values) =>
+            this.setState({ watched: result })
+        );
+    }
 
-  render() {
-    return (
-      <React.Fragment>
-        {this.state.watched.length > 0 ? (
-          <React.Fragment>
-            <h2>Continue watching</h2>
-            <div className="horizontal-list">
-              {this.state.watched.map((watch) => (
-                <Link
-                  to={`/series/${watch.series}/${watch.season}/${watch.episode}/`}
-                  key={`${watch.series}/${watch.season}/${watch.episode}`}
-                >
-                  <Card
-                    image={
-                      this.state.series[watch.series] &&
-                      `url('https://img.youtube.com/vi/${
-                        this.state.series[watch.series].seasons[watch.season][
-                          watch.episode
-                        ].YTID
-                      }/mqdefault.jpg')`
-                    }
-                    progress={`${Math.round(
-                      (watch.time / watch.totalTime) * 100
-                    )}%`}
-                  />
-                </Link>
-              ))}
-            </div>
-          </React.Fragment>
-        ) : null}
-        <h2>Recommended</h2>
-        <div className="horizontal-list">
-          {Object.keys(this.state.series).map((key) => (
-            <Link to={`/series/${key}/`} key={key}>
-              <Poster
-                marked={History.isUnwatched(
-                  key,
-                  undefined,
-                  undefined,
-                  this.state.series[key]
-                )}
-                image={"url('" + this.state.series[key].image + "')"}
-              />
-            </Link>
-          ))}
-        </div>
-        <h2>Dramas</h2>
-        <div className="horizontal-list">
-          <Poster marked />
-          <Poster marked />
-          <Poster marked />
-          <Poster marked />
-        </div>
-        <h2>Recently added</h2>
-        <div className="horizontal-list">
-          <Poster marked />
-          <Poster marked />
-          <Poster marked />
-          <Poster marked />
-        </div>
-        <h2>YouTube Originals</h2>
-        <div className="horizontal-list">
-          <Poster marked />
-          <Poster marked />
-          <Poster marked />
-          <Poster marked />
-        </div>
-        <h2>Grid</h2>
-        <div className="grid-list">
-          <Poster
-            marked
-            image={
-              "url(https://www.themoviedb.org/t/p/original/6JOlFNNVXprII3fSX0t330GHkau.jpg)"
-            }
-          />
-          <Poster
-            marked
-            image={
-              "url(https://www.themoviedb.org/t/p/original/nDMtG1NwnpA3rPDld38BvWRsVf.jpg)"
-            }
-          />
-          <Poster
-            marked
-            image={
-              "url(https://www.themoviedb.org/t/p/original/dgMstu4lbtR9hznDYFhU7QR8cZr.jpg)"
-            }
-          />
-          <Poster marked />
-          <Poster marked />
-          <Poster marked />
-          <Poster marked />
-          <Poster marked />
-          <Poster marked />
-          <Poster marked />
-        </div>
-      </React.Fragment>
-    );
-  }
+    componentDidCatch(error, errorInfo) {
+        // You can also log the error to an error reporting service
+        console.log(error, errorInfo);
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {this.state.watched.length > 0 ? (
+                    <React.Fragment>
+                        <h2>Continue watching</h2>
+                        <div className="horizontal-list">
+                            {this.state.watched.map((watch) => (
+                                <Link
+                                    to={`/series/${watch.series}/${watch.season}/${watch.episode}/`}
+                                    key={`${watch.series}/${watch.season}/${watch.episode}`}
+                                >
+                                    <Card
+                                        image={
+                                            this.state.series[watch.series] &&
+                                            `url('https://img.youtube.com/vi/${
+                                                this.state.series[watch.series]
+                                                    .seasons[watch.season][
+                                                    watch.episode
+                                                ].YTID
+                                            }/mqdefault.jpg')`
+                                        }
+                                        progress={`${Math.round(
+                                            (watch.time / watch.totalTime) * 100
+                                        )}%`}
+                                    />
+                                </Link>
+                            ))}
+                        </div>
+                    </React.Fragment>
+                ) : null}
+                <h2>Recommended</h2>
+                <div className="horizontal-list">
+                    {Object.keys(this.state.series).map((key) => (
+                        <Link to={`/series/${key}/`} key={key}>
+                            <Poster
+                                marked={History.isUnwatched(
+                                    key,
+                                    undefined,
+                                    undefined,
+                                    this.state.series[key]
+                                )}
+                                image={
+                                    "url('https://image.tmdb.org/t/p/w300/" +
+                                    this.state.series[key].poster +
+                                    "')"
+                                }
+                                title={this.state.series[key].title}
+                            />
+                        </Link>
+                    ))}
+                </div>
+                <h2>Dramas</h2>
+                <div className="horizontal-list">
+                    <Poster marked />
+                    <Poster marked />
+                    <Poster marked />
+                    <Poster marked />
+                </div>
+                <h2>Recently added</h2>
+                <div className="horizontal-list">
+                    <Poster marked />
+                    <Poster marked />
+                    <Poster marked />
+                    <Poster marked />
+                </div>
+                <h2>YouTube Originals</h2>
+                <div className="horizontal-list">
+                    <Poster marked />
+                    <Poster marked />
+                    <Poster marked />
+                    <Poster marked />
+                </div>
+                <h2>Grid</h2>
+                <div className="grid-list">
+                    <Poster
+                        marked
+                        image={
+                            "url(https://www.themoviedb.org/t/p/original/6JOlFNNVXprII3fSX0t330GHkau.jpg)"
+                        }
+                    />
+                    <Poster
+                        marked
+                        image={
+                            "url(https://www.themoviedb.org/t/p/original/nDMtG1NwnpA3rPDld38BvWRsVf.jpg)"
+                        }
+                    />
+                    <Poster
+                        marked
+                        image={
+                            "url(https://www.themoviedb.org/t/p/original/dgMstu4lbtR9hznDYFhU7QR8cZr.jpg)"
+                        }
+                    />
+                    <Poster marked />
+                    <Poster marked />
+                    <Poster marked />
+                    <Poster marked />
+                    <Poster marked />
+                    <Poster marked />
+                    <Poster marked />
+                </div>
+            </React.Fragment>
+        );
+    }
 }
