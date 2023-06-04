0203e40bfeee5f0f3ba2711b4e68f2745d8ad31d
Anders Pedersen <hot415@hotmail.com>
2023-06-05 01:19:09 +0200

    Removed psudo-laravel database files for firebase

--- src/Database/FirebaseConnection.js
+++
@@ -1,9 +0,0 @@
-import Connection from "../../merchant/Illuminate/Database/Connection";
-import FirebaseGrammar from "./Query/Grammars/FirebaseGrammar";
-
-export default class FirebaseConnection extends Connection {
-  //schemaGrammar = FirebaseGrammar;
-  getDefaultQueryGrammar() {
-    return new FirebaseGrammar();
-  }
-}
