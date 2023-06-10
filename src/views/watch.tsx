e9a069695567d183bb465eb863afbae252ebe7c4
Anders Pedersen <hot415@hotmail.com>
2023-06-10 23:02:24 +0200

    Update watch.tsx

--- src/views/watch.tsx
+++ src/views/watch.tsx
@@ -1,27 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Video from "./video";
-import { withRouter } from "react-router";
-import { HashRouter } from "react-router-dom";
+import { RouteComponentProps } from "react-router";
 
 // https://developers.google.com/youtube/player_parameters
 // https://stackoverflow.com/questions/40353582/modestbranding-not-working-for-youtube-embed#answer-52987295
 
-export default class Watch extends React.Component {
-  render() {
-    return (
-      <Video
-        id="ytplayer"
-        title="ytplayer"
-        autoplay
-        enablejsapi
-        controls
-        disablekb
-        fs
-        modestbranding
-        origin="https://kztbl.codesandbox.io"
-        VIDEO_ID={this.props.match.params.id}
-      />
-    );
-  }
+export default class Watch extends React.Component<
+    RouteComponentProps<{ id: string }>
+> {
+    render() {
+        return (
+            <Video
+                id="ytplayer"
+                title="ytplayer"
+                autoplay
+                enablejsapi
+                controls
+                disablekb
+                fs
+                modestbranding
+                origin="https://kztbl.codesandbox.io"
+                VIDEO_ID={this.props.match.params.id}
+            />
+        );
+    }
 }
