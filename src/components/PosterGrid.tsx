8c27836aba6619eb42ee794dfdbd623dffd6450f
Anders Pedersen <hot415@hotmail.com>
2024-11-01 00:01:30 +0100

    Changed PosterGrid component from class to functional implementation

--- src/components/PosterGrid.tsx
+++ src/components/PosterGrid.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "./PosterGrid.css";
 
-export default class PosterGrid extends React.Component<
-    React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>
-> {
-    /**
-     * render
-     */
-    public render() {
-        return (
-            <div className="postergrid" {...this.props}>
-                {this.props.children}
-            </div>
-        );
-    }
+export default function PosterGrid({
+    children,
+    ...props
+}: React.PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) {
+    return (
+        <div className="postergrid" {...props}>
+            {children}
+        </div>
+    );
 }
