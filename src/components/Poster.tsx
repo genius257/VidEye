9fd81423ee5574a1d154873111c1353c90dcdb0d
Anders Pedersen <hot415@hotmail.com>
2024-11-02 03:32:43 +0100

    Refactored Poster component from class to function based component

--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, {
+    CSSProperties,
+    /*MouseEvent,*/ useCallback,
+    useRef,
+    useState
+} from "react";
 import "./Poster.css";
 import ContextMenu, { ContextMenuItem } from "../contextMenu";
 
@@ -8,93 +13,86 @@ type PosterProps = {
     marked?: boolean;
 };
 
-type PosterState = {
-    counter: null;
-    //watched: boolean;
-    progress?: number;
-};
+export default function Poster({ title, image, marked }: PosterProps) {
+    const [counter, setCounter] = useState(null);
+    const [progress, setProgress] = useState(0);
 
-export default class Poster extends React.Component<PosterProps, PosterState> {
-    state: PosterState = {
-        counter: null
-        //watched: false
-    };
+    const ref = useRef<HTMLDivElement>(null);
 
-    root: HTMLDivElement | null = null;
+    const contextMenuCallback = useCallback(
+        (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+            e.preventDefault();
 
-    onContextMenu(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-        e.preventDefault();
+            const clickX = e.clientX;
+            const clickY = e.clientY;
+            const screenW = window.innerWidth;
+            const screenH = window.innerHeight;
+            const rootW = ref.current?.offsetWidth ?? 0;
+            const rootH = ref.current?.offsetHeight ?? 0;
 
-        const clickX = e.clientX;
-        const clickY = e.clientY;
-        const screenW = window.innerWidth;
-        const screenH = window.innerHeight;
-        const rootW = this.root?.offsetWidth ?? 0;
-        const rootH = this.root?.offsetHeight ?? 0;
+            const right = screenW - clickX > rootW;
+            const left = !right;
+            const top = screenH - clickY > rootH;
+            const bottom = !top;
 
-        const right = screenW - clickX > rootW;
-        const left = !right;
-        const top = screenH - clickY > rootH;
-        const bottom = !top;
+            ContextMenu.add(
+                <>
+                    <ContextMenuItem
+                        onClick={(e) => console.log("mark as watched")}
+                    >
+                        Mark as watched
+                    </ContextMenuItem>
+                    <ContextMenuItem>Mark as unwatched</ContextMenuItem>
+                    <ContextMenuItem disabled>Details</ContextMenuItem>
+                    <ContextMenuItem>Something else</ContextMenuItem>
+                </>,
+                e.pageX,
+                e.pageY
+            );
 
-        ContextMenu.add(
-            <React.Fragment>
-                <ContextMenuItem
-                    onClick={(e) => console.log("mark as watched")}
-                >
-                    Mark as watched
-                </ContextMenuItem>
-                <ContextMenuItem>Mark as unwatched</ContextMenuItem>
-                <ContextMenuItem disabled>Details</ContextMenuItem>
-                <ContextMenuItem>Something else</ContextMenuItem>
-            </React.Fragment>,
-            e.pageX,
-            e.pageY
-        );
+            //this.root.style.display = "absolute";
 
-        //this.root.style.display = "absolute";
+            if (right) {
+                //this.root.style.left = `${clickX + 5}px`;
+            }
 
-        if (right) {
-            //this.root.style.left = `${clickX + 5}px`;
-        }
+            if (left) {
+                //this.root.style.left = `${clickX - rootW - 5}px`;
+            }
 
-        if (left) {
-            //this.root.style.left = `${clickX - rootW - 5}px`;
-        }
+            if (top) {
+                //this.root.style.top = `${clickY + 5}px`;
+            }
 
-        if (top) {
-            //this.root.style.top = `${clickY + 5}px`;
-        }
+            if (bottom) {
+                //this.root.style.top = `${clickY - rootH - 5}px`;
+            }
 
-        if (bottom) {
-            //this.root.style.top = `${clickY - rootH - 5}px`;
-        }
+            //console.log("onContextMenu");
+        },
+        []
+    );
 
-        //console.log("onContextMenu");
-    }
+    var className = ["poster", marked ? "marked" : ""]
+        .filter((v) => v)
+        .join(" ");
 
-    render() {
-        var className = ["poster", this.props.marked ? "marked" : ""]
-            .filter((v) => v)
-            .join(" ");
-
-        return (
-            <div
-                ref={(ref) => (this.root = ref)}
-                className={className}
-                style={
-                    {
-                        backgroundImage: this.props.image,
-                        "--card-progress": this.state.progress
-                    } as React.CSSProperties
-                }
-                onContextMenu={this.onContextMenu.bind(this)}
-                title={this.props.title}
-            >
-                {this.state.counter !== null || !this.props.marked ? (
-                    <div className="posterCounter"></div>
-                ) : null}
-            </div>
-        );
-    }
+    return (
+        <div
+            ref={ref}
+            className={className}
+            style={
+                {
+                    backgroundImage: image,
+                    "--card-progress": progress
+                } as CSSProperties
+            }
+            onContextMenu={contextMenuCallback}
+            title={title}
+        >
+            {counter !== null || !marked ? (
+                <div className="posterCounter"></div>
+            ) : null}
+        </div>
+    );
 }
