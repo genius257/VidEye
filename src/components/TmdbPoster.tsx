438f4b7fb7060204ade4e4d929ff5aefb24c3f54
Anders Pedersen <hot415@hotmail.com>
2024-11-02 03:28:14 +0100

    Refactored TmdbPoster component from class to function type component

--- src/components/TmdbPoster.tsx
+++ src/components/TmdbPoster.tsx
@@ -1,12 +1,5 @@
-import React from "react";
 import Poster from "./Poster";
 
-export default class TmdbPoster extends React.Component<{ image: string }> {
-    render() {
-        return (
-            <Poster
-                image={`https://image.tmdb.org/t/p/w300/${this.props.image.trim()}`}
-            />
-        );
-    }
+export default function TmdbPoster({ image }: { image: string }) {
+    return <Poster image={`https://image.tmdb.org/t/p/w300/${image.trim()}`} />;
 }
