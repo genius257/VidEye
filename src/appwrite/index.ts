6b2049c5bdd7b24ea042f7a73d52f12fc4b8a879
Anders Pedersen <hot415@hotmail.com>
2024-12-13 00:40:11 +0100

    Added database ids to the appwrite exported collection

--- src/appwrite/index.ts
+++ src/appwrite/index.ts
@@ -8,8 +8,17 @@ const account = new Account(client);
 
 const databases = new Databases(client);
 
+const databaseIds = {
+    series: "671ec7fb000b3517b7e6",
+    seasons: "671ecb800036fac028e0",
+    movies: "671ec5e1002943b28df8",
+    history: "671eca420003af618870",
+    episodes: "671ec5a900219da44149",
+};
+
 export {
     client,
     account,
     databases,
+    databaseIds,
 };
