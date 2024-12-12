acca4d468c76468297989a0ed7c599a30d5a0e01
Anders Pedersen <hot415@hotmail.com>
2024-12-12 14:40:13 +0100

    Changed Avatar component from class to functional component

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,24 +1,37 @@
 import md5 from "blueimp-md5";
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import "./Avatar.css";
+import { useUser } from "@/appwrite/context/user";
+import { account } from "@/appwrite";
 
-export default class Avatar extends Component {
-    render() {
-        const email = this.props?.auth?.user?.email;
-        const hash =
-            email == null
-                ? "00000000000000000000000000000000"
-                : md5(email.trim().toLowerCase());
-        const photoURL = `https://www.gravatar.com/avatar/${hash}`;
+export default function Avatar() {
+    const user = useUser();
+    const [photoURL, setPhotoURL] = useState(
+        "https://www.gravatar.com/avatar/"
+    );
+    const [displayName, setDisplayName] = useState("Anonymous");
+    useEffect(() => {
+        if (user?.current == null) {
+            setPhotoURL("https://www.gravatar.com/avatar/");
+            setDisplayName("Anonymous");
+        }
 
-        //const photoURL = this.props?.auth?.user?.photoURL;
-        const displayName = this.props?.auth?.user?.displayName;
-        return (
-            <div
-                className="avatar"
-                style={{ backgroundImage: `url(${photoURL})` }}
-                title={displayName}
-            />
-        );
-    }
+        account.get().then((value) => {
+            const email = value.email;
+            const hash =
+                email == null
+                    ? "00000000000000000000000000000000"
+                    : md5(email.trim().toLowerCase());
+            setPhotoURL(`https://www.gravatar.com/avatar/${hash}`);
+            setDisplayName(value.name);
+        });
+    }, [user?.current]);
+
+    return (
+        <div
+            className="avatar"
+            style={{ backgroundImage: `url(${photoURL})` }}
+            title={displayName}
+        />
+    );
 }
