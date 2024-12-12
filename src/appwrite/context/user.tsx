82a53f1ff1f8448f0f57d56aa3b94264c825b331
Anders Pedersen <hot415@hotmail.com>
2024-12-13 00:39:29 +0100

    Updated user context, to work with OTP methods
    A quick implementation for now, to get everything to work

--- src/appwrite/context/user.tsx
+++ src/appwrite/context/user.tsx
@@ -10,9 +10,12 @@ import { ID, Models } from "appwrite";
 
 const UserContext = createContext<{
     current: Models.Session | Models.User<Models.Preferences> | null;
+    token: Models.Token | null;
     login: (email: string, password: string) => Promise<void>;
     logout: () => Promise<void>;
     register: (email: string, password: string) => Promise<void>;
+    createEmailToken: (email: string) => Promise<Models.Token>;
+    createSession: (secret: string) => Promise<Models.Session>;
 } | null>(null);
 
 export function useUser() {
@@ -23,6 +26,8 @@ export function UserProvider(props: PropsWithChildren) {
     const [user, setUser] = useState<
         Models.Session | Models.User<Models.Preferences> | null
     >(null);
+
+    const [token, setToken] = useState<Models.Token | null>(null);
 
     async function login(email: string, password: string) {
         const loggedIn = await account.createEmailPasswordSession(
@@ -43,6 +48,33 @@ export function UserProvider(props: PropsWithChildren) {
         await login(email, password);
     }
 
+    async function createEmailToken(email: string) {
+        return await account.createEmailToken(ID.unique(), email).then(
+            (value) => {
+                setToken(value);
+                return value;
+            },
+            (reason) => {
+                throw new Error(reason.message); //FIXME: handle error
+            }
+        );
+    }
+
+    async function createSession(secret: string) {
+        if (token == null) {
+            throw new Error("No token found");
+        }
+        return await account.createSession(token?.userId, secret).then(
+            (value) => {
+                setUser(value);
+                return value;
+            },
+            (reason) => {
+                throw new Error(reason.message); //FIXME: handle error
+            }
+        );
+    }
+
     async function init() {
         try {
             const loggedIn = await account.get();
@@ -58,7 +90,15 @@ export function UserProvider(props: PropsWithChildren) {
 
     return (
         <UserContext.Provider
-            value={{ current: user, login, logout, register }}
+            value={{
+                current: user,
+                token,
+                login,
+                logout,
+                register,
+                createEmailToken,
+                createSession
+            }}
         >
             {props.children}
         </UserContext.Provider>
